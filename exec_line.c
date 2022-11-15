8f8d94d32d4b6a8db3f8afe8a6fdd25b28f20849
baay02 <baaybilya25@yahoo.com>
2022-11-15 11:57:45 -0800

    team project

--- exec_line.c
+++ exec_line.c
@@ -1,26 +1,16 @@
 #include "main.h"
 
-
-
 /**
-
  * exec_line - finds builtins and commands
-
  *
-
  * @datash: data relevant (args)
-
  * Return: 1 on success.
-
  */
 
 int exec_line(data_shell *datash)
-
 {
 
 	int (*builtin)(data_shell *datash);
-
-
 
 	if (datash->args[0] == NULL)
 
