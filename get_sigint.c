061a91f483ab2e1b0b5eb8cd7bc5ccdafd56b8cf
baay02 <baaybilya25@yahoo.com>
2022-11-15 02:10:52 -0800

    team project

--- get_sigint.c
+++ get_sigint.c
@@ -3,15 +3,11 @@
 
 
 /**
-
  * get_sigint - Handle the crtl + c call in prompt
-
  * @sig: Signal handler
-
  */
 
 void get_sigint(int sig)
-
 {
 
 	(void)sig;
--- read_line.c
+++ read_line.c
@@ -1,21 +1,13 @@
 #include "main.h"
 
-
-
 /**
-
  * read_line - reads the input string.
-
  *
-
  * @i_eof: return value of getline function
-
  * Return: input string
-
  */
 
 char *read_line(int *i_eof)
-
 {
 
 	char *input = NULL;
