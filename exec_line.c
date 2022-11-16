c27f210b2a07e8f8cfd9c4f6b3b50290a6279465
baay02 <baaybilya25@yahoo.com>
2022-11-16 00:54:55 -0800

    team project

--- exec_line.c
+++ exec_line.c
@@ -6,21 +6,18 @@
  * @datash: data relevant (args)
  * Return: 1 on success.
  */
-
 int exec_line(data_shell *datash)
 {
-
 	int (*builtin)(data_shell *datash);
 
 	if (datash->args[0] == NULL)
-
 		return (1);
 
 	builtin = get_builtin(datash->args[0]);
 
 	if (builtin != NULL)
 		return (builtin(datash));
-	return (cmd_exec(datash));
 
+	return (cmd_exec(datash));
 }
 
